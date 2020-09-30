febd64000c9f4467f39cfa7b0ae5d899a2734bfa
Aditya Kuroodi <40967218+akuroodi@users.noreply.github.com>
2020-09-30 14:21:55 -0700

    Add files via upload

---
+++ 6001 - Intro to CS with Python/ps3_hangman.py
@@ -0,0 +1,139 @@
+# Hangman game
+#
+
+# -----------------------------------
+# Helper code
+# You don't need to understand this helper code,
+# but you will have to know how to use the functions
+# (so be sure to read the docstrings!)
+
+import random
+
+WORDLIST_FILENAME = "/Users/adityakuroodi/Documents/PythonClass/words.txt"
+
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print("Loading word list from file...")
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r')
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = line.split()
+    print("  ", len(wordlist), "words loaded.")
+    return wordlist
+
+def chooseWord(wordlist):
+    """
+    wordlist (list): list of words (strings)
+
+    Returns a word from wordlist at random
+    """
+    return random.choice(wordlist)
+
+# end of helper code
+# -----------------------------------
+
+# Load the list of words into the variable wordlist
+# so that it can be accessed from anywhere in the program
+wordlist = loadWords()
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+   
+    y = [False for x in set(secretWord) if x not in lettersGuessed]
+    return all(y)
+
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    return "".join(x if x in lettersGuessed else ' _ ' for x in list(secretWord) )
+
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed. In alphabetical order.
+    '''
+    import string
+    x = string.ascii_lowercase
+
+    y = "".join(z for z in list(x) if z not in lettersGuessed)
+    return y
+
+def hangman(secretWord):
+    '''
+    secretWord: string, the secret word to guess.
+
+    Starts up an interactive game of Hangman.
+
+    * At the start of the game, let the user know how many 
+      letters the secretWord contains.
+
+    * Ask the user to supply one guess (i.e. letter) per round.
+
+    * The user should receive feedback immediately after each guess 
+      about whether their guess appears in the computers word.
+
+    * After each round, you should also display to the user the 
+      partially guessed word so far, as well as letters that the 
+      user has not yet guessed.
+
+    Follows the other limitations detailed in the problem write-up.
+    '''
+    print("Welcome to the game, Hangman!")
+    print("I am thinking of a word that is " + str(len(secretWord)) + " letters long.")
+    print("-------------")
+
+    numGuesses = 8          # every game starts with 8 guesses
+    lettersGuessed = []     # empty list to initialize which letters have been guessed
+
+    while(numGuesses > 0):
+      
+      if(isWordGuessed(secretWord, lettersGuessed)):
+        print("Congratulations, you won!")
+        break
+      
+      print("You have " + str(numGuesses) + " guesses left.")
+
+      print("Available letters: " + getAvailableLetters(lettersGuessed))
+
+      x = input("Please guess a letter: ")
+      
+      if (x in lettersGuessed):
+        print("Oops! You've already guessed that letter: " + getGuessedWord(secretWord, lettersGuessed))
+        print("-------------")
+        continue   
+      else:
+        lettersGuessed.append(x)
+      
+      if (x in secretWord):
+        print("Good guess: " + getGuessedWord(secretWord, lettersGuessed))
+        print("-------------")
+        continue
+
+      else:
+        print("Oops! That letter is not in my word: " + getGuessedWord(secretWord, lettersGuessed))
+        print("-------------")
+        numGuesses -= 1
+      
+    if (numGuesses == 0):
+      print("Sorry, you ran out of guesses. The word was " + secretWord + ".")
+
+
+
+secretWord = chooseWord(wordlist).lower()
+hangman(secretWord)
---
+++ 6001 - Intro to CS with Python/ps5/ps6.py
@@ -0,0 +1,290 @@
+import string
+
+### DO NOT MODIFY THIS FUNCTION ###
+def load_words(file_name):
+    '''
+    file_name (string): the name of the file containing 
+    the list of words to load    
+    
+    Returns: a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    '''
+    print('Loading word list from file...')
+    # inFile: file
+    in_file = open(file_name, 'r')
+    # line: string
+    line = in_file.readline()
+    # word_list: list of strings
+    word_list = line.split()
+    print('  ', len(word_list), 'words loaded.')
+    in_file.close()
+    return word_list
+
+### DO NOT MODIFY THIS FUNCTION ###
+def is_word(word_list, word):
+    '''
+    Determines if word is a valid word, ignoring
+    capitalization and punctuation
+
+    word_list (list): list of words in the dictionary.
+    word (string): a possible word.
+    
+    Returns: True if word is in word_list, False otherwise
+
+    Example:
+    >>> is_word(word_list, 'bat') returns
+    True
+    >>> is_word(word_list, 'asdf') returns
+    False
+    '''
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
+    return word in word_list
+
+### DO NOT MODIFY THIS FUNCTION ###
+def get_story_string():
+    """
+    Returns: a joke in encrypted text.
+    """
+    f = open("story.txt", "r")
+    story = str(f.read())
+    f.close()
+    return story
+
+WORDLIST_FILENAME = '/Users/adityakuroodi/Documents/PythonClass/Problem Sets/ps5/words.txt'
+
+class Message(object):
+    ### DO NOT MODIFY THIS METHOD ###
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+                
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+        
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class
+        
+        Returns: a COPY of self.valid_words
+        '''
+        return self.valid_words[:]
+        
+    def build_shift_dict(self, shift):
+        '''
+        Creates a dictionary that can be used to apply a cipher to a letter.
+        The dictionary maps every uppercase and lowercase letter to a
+        character shifted down the alphabet by the input shift. The dictionary
+        should have 52 keys of all the uppercase letters and all the lowercase
+        letters only.        
+        
+        shift (integer): the amount by which to shift every letter of the 
+        alphabet. 0 <= shift < 26
+
+        Returns: a dictionary mapping a letter (string) to 
+                 another letter (string). 
+        '''
+        lowers = {}
+        uppers = {}
+        lref = string.ascii_lowercase *2
+        uref = string.ascii_uppercase *2
+      
+        # Setup the initial dictionaries for lower and upper case
+        for letter in (string.ascii_lowercase):
+            lowers.setdefault(letter, letter)
+        for letter in (string.ascii_uppercase):
+            uppers.setdefault(letter, letter)
+
+        # Shift the lowercase letters
+        for key in lowers.keys():
+            Newpos = lref.find(key) + shift
+            lowers[key] = lref[Newpos]
+
+        # Shift the uppercase letters
+        for key in uppers.keys():
+            Newpos = uref.find(key) + shift
+            uppers[key] = uref[Newpos]
+   
+      
+        # Return the joint, shifted, dictionaries
+        lowers.update(uppers)
+        return lowers
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift        
+        
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
+
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        copymessage = list ( self.get_message_text() )
+        D = self.build_shift_dict(shift)
+        encrypted = ""
+
+        for letter in copymessage:
+            # Do not apply cipher to numbers, spaces, or punctuation
+            if (letter.isdigit() or letter.isspace() or string.punctuation.find(letter) != -1 ): 
+                encrypted += letter
+                continue
+           
+            # Apply the cipher to all letters in the message to make an encyrpted message
+            encrypted += D[letter]
+        
+        return encrypted
+
+class PlaintextMessage(Message):
+    def __init__(self, text, shift):
+        '''
+        Initializes a PlaintextMessage object        
+        
+        text (string): the message's text
+        shift (integer): the shift associated with this message
+
+        A PlaintextMessage object inherits from Message and has five attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+            self.shift (integer, determined by input shift)
+            self.encrypting_dict (dictionary, built using shift)
+            self.message_text_encrypted (string, created using shift)
+
+        Hint: consider using the parent class constructor so less 
+        code is repeated
+        '''
+
+        Message.__init__(self, text)
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
+
+    def get_shift(self):
+        '''
+        Used to safely access self.shift outside of the class
+        
+        Returns: self.shift
+        '''
+        return self.shift
+    def get_encrypting_dict(self):
+        '''
+        Used to safely access a copy self.encrypting_dict outside of the class
+        
+        Returns: a COPY of self.encrypting_dict
+        '''
+        return self.encrypting_dict.copy()
+
+    def get_message_text_encrypted(self):
+        '''
+        Used to safely access self.message_text_encrypted outside of the class
+        
+        Returns: self.message_text_encrypted
+        '''
+        return self.message_text_encrypted
+
+    def change_shift(self, shift):
+        '''
+        Changes self.shift of the PlaintextMessage and updates other 
+        attributes determined by shift (ie. self.encrypting_dict and 
+        message_text_encrypted).
+        
+        shift (integer): the new shift that should be associated with this message.
+        0 <= shift < 26
+
+        Returns: nothing
+        '''
+        self.shift = shift
+        self.encrypting_dict = self.build_shift_dict(shift)
+        self.message_text_encrypted = self.apply_shift(shift)
+
+
+class CiphertextMessage(Message):
+    def __init__(self, text):
+        '''
+        Initializes a CiphertextMessage object
+                
+        text (string): the message's text
+
+        a CiphertextMessage object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words)
+        '''
+        Message.__init__(self, text)
+    
+    
+    
+    def decrypt_message(self):
+        '''
+        Decrypt self.message_text by trying every possible shift value
+        and find the "best" one. We will define "best" as the shift that
+        creates the maximum number of real words when we use apply_shift(shift)
+        on the message text. If s is the original shift value used to encrypt
+        the message, then we would expect 26 - s to be the best shift value 
+        for decrypting it.
+
+        Note: if multiple shifts are  equally good such that they all create 
+        the maximum number of you may choose any of those shifts (and their
+        corresponding decrypted messages) to return
+
+        Returns: a tuple of the best shift value used to decrypt the message
+        and the decrypted message text using that shift value
+        '''
+        Wordcounter = 0
+        maxwords = 0
+        collection = {}
+        
+
+
+        for s in range(27):
+            trialString = self.apply_shift(s)
+            Wordcounter = 0
+
+            for word in trialString.split(" "):
+                if (word in self.valid_words): 
+                    Wordcounter += 1
+            
+            if (Wordcounter > maxwords):
+                maxwords = Wordcounter
+                collection.update({s : self.apply_shift(s) })
+        
+        listt = [ (k,v) for k, v in collection.items() ]
+
+        return listt[0]
+
+        
+
+
+
+
+
+    
+# #Example test case (CiphertextMessage)
+# ciphertext = CiphertextMessage('jgnnq')
+# print('Expected Output:', (24, 'hello'))
+# print('Actual Output:', ciphertext.decrypt_message())
+
+flab = CiphertextMessage("jgnnq")
+
+
+
+print(flab.decrypt_message())
